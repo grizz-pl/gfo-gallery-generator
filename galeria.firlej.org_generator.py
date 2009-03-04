aaa2cbbe791b155612b3ba1e1dc983902a36c822
Witold Firlej <witek.firlej@gmail.com>
2009-03-04 08:57:24 +0100

    initial

---
+++ galeria.firlej.org_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#galeria.firlej.org generator ver. 0.1 by grizz - Witek Firlej http://grizz.pl
+
+__project__      = "galeria.firlej.org generator"
+__author__    = "Witold Firlej (http://grizz.pl)"
+__version__   = "0.1"
+__license__   = "GPL"
+__copyright__ = "Witold Firlej"
+
+import os,sys
